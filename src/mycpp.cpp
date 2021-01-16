bddfb3f9e37ba201ad4aa2060458ada2d7908edb
RenjieChiang <1037253670@qq.com>
2021-01-16 16:21:54 +0800

    20210116pm4

---
+++ src/mycpp.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+
+// void showValue( int a , int b  = 20 );
+// int func(int a , int );
+int main()
+{
+    int c = 10;
+    // showValue(c);
+    int * const p = &c;
+    c++;
+    (*p)++;
+    std::cout<<*p;
+    // std::cout<<func(c,10)<<std::endl;
+    return 0;
+}
+
+// void showValue( int a , int b  )//只能函数声明有default parameter value
+// {
+//     std::cout<<a +  b<<std::endl;
+// }
+// int func (int a, int b)
+// {
+//     return a+b ;
+// }
