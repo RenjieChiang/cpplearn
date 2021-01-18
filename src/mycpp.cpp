f2062c326ee09fbe810f43a6ed0d28fd99797a76
RenjieChiang <1037253670@qq.com>
2021-01-18 18:31:49 +0800

    111

--- src/mycpp.cpp
+++ src/mycpp.cpp
@@ -1,19 +1,17 @@
-#include "iostream"
-
+// #include "iostream"
 // void showValue( int a , int b  = 20 );
 // int func(int a , int );
-int main()
-{
-    int c = 10;
-    // showValue(c);
-    int * const p = &c;
-    c++;
-    (*p)++;
-    std::cout<<*p;
-    // std::cout<<func(c,10)<<std::endl;
-    return 0;
-}
-
+// int main()
+// {
+//     int c = 10;
+//     // showValue(c);
+//     int * const p = &c;
+//     c++;
+//     (*p)++;
+//     std::cout<<*p;
+//     // std::cout<<func(c,10)<<std::endl;
+//     return 0;
+// }
 // void showValue( int a , int b  )//只能函数声明有default parameter value
 // {
 //     std::cout<<a +  b<<std::endl;
@@ -22,3 +20,10 @@ int main()
 // {
 //     return a+b ;
 // }
+#include "mycpp.h"
+int * add(int a , int b)
+{
+    int *p = new int;
+    *p = a + b;
+    return p;
+}
