17b7df48013b3d99bc6cdd7ef6e8ac41408a24b0
RenjieChiang <1037253670@qq.com>
2021-03-03 14:46:45 +0800

    win10509 2021030315pm

--- cmake-build-debug/CMakeFiles/my_cpp_learn.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/my_cpp_learn.dir/DependInfo.cmake
@@ -4,15 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/tianbot/my_cpp_learn/src/mycpp.cpp" "/home/tianbot/my_cpp_learn/cmake-build-debug/CMakeFiles/my_cpp_learn.dir/src/mycpp.cpp.o"
-  "/home/tianbot/my_cpp_learn/src/mycppmain.cpp" "/home/tianbot/my_cpp_learn/cmake-build-debug/CMakeFiles/my_cpp_learn.dir/src/mycppmain.cpp.o"
+  "C:/Users/10372/CLionProjects/cpplearn/src/mycpp.cpp" "C:/Users/10372/CLionProjects/cpplearn/cmake-build-debug/CMakeFiles/my_cpp_learn.dir/src/mycpp.cpp.obj"
+  "C:/Users/10372/CLionProjects/cpplearn/src/mycppmain.cpp" "C:/Users/10372/CLionProjects/cpplearn/cmake-build-debug/CMakeFiles/my_cpp_learn.dir/src/mycppmain.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../src"
-  "/usr/lib"
   )
 
 # Targets to which this target links.
